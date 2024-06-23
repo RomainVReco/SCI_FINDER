f38b04ae0ab390f903688bd2e9b768438abdb4fb
Romain VORLET <romain.vorlet@gmail.com>
2024-06-23 22:42:19 +0200

    refacto autowiring

--- src/Controller/SciController.php
+++ src/Controller/SciController.php
@@ -22,11 +22,11 @@ class SciController extends AbstractController
 
     public function __construct(SciRepository $sciRepo, SerializerInterface $serializer) 
     {
-        $this->$sciRepo = $sciRepo;
-        $this->$serializer = $serializer;
+        $this->sciRepo = $sciRepo;
+        $this->serializer = $serializer;
     }
 
-    #[Route('/api/sci', name:'sci', methods:['GET'])]
+    #[Route('/api/sci', name:'AllSCI', methods:['GET'])]
     public function getAllSCI(): JsonResponse
     {
         $sciList = $this->sciRepo->findAll();
@@ -34,40 +34,41 @@ class SciController extends AbstractController
         return new JsonResponse($jsonSciList, Response::HTTP_OK, [], true);
     }
 
-    #[Route('/api/sci/{id}', name:'sci', methods:['GET'])]
-    public function getSingleSCI(int $id, SciRepository $sciRepo, SerializerInterface $serializer): JsonResponse
+    #[Route('/api/sci/{id}', name:'singleSCI', methods:['GET'])]
+    public function getSingleSCI(int $id): JsonResponse
     {
-        $sci = $sciRepo->find($id);
+        $sci = $this->sciRepo->find($id);
         if ($sci) {
-            $jsonSCI = $serializer->serialize($sci, 'json',);
+            $jsonSCI = $this->serializer->serialize($sci, 'json',);
             return new JsonResponse($jsonSCI, Response::HTTP_OK, [], true);
         }
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
-    #[Route('api/sci/partial/{id}', name:'sci', methods:['GET'])]
-    public function getSciFromPartialId(int $id, SciRepository $sciRepo, SerializerInterface $serializer): JsonResponse
+    #[Route('api/sci/partial/{id}', name:'partialSCI', methods:['GET'])]
+    public function getSciFromPartialId(int $id): JsonResponse
     {
-        $sci = $sciRepo->findByPartialId($id);
+        $sci = $this->sciRepo->findByPartialId($id);
         if ($sci) {
-            $jsonSCI = $serializer->serialize($sci, 'json');
+            $jsonSCI = $this->serializer->serialize($sci, 'json');
             return new JsonResponse($jsonSCI, Response::HTTP_OK, [], true);
         }
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
-    // #[Route('/api/sci', name:'sci', methods:['POST'])]
-    // public function createSCI (Request $request, SerializerInterface $serializer, EntityManagerInterface $em, 
-    // UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator): JsonResponse
-    //  {
-    //     $sci = $serializer->deserialize($request->getContent(), Sci::class, 'json');
-    //     $em->persist($sci);
-    //     $em->flush();
+    #[Route('/api/sci', name:'createSCI', methods:['POST'])]
+    public function createSCI (Request $request, EntityManagerInterface $em, 
+    UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator): JsonResponse
+     {
+        $sci = $this->serializer->deserialize($request->getContent(), Sci::class, 'json');
+        $em->persist($sci);
+        $em->flush();
 
-    //     $jsonSCI = $sci ;
+        $jsonSCI = $this->serializer->serialize($sci, 'json');
+        $location = $urlGenerator->generate('singleSCI', ['id' => $sci->getId()], UrlGeneratorInterface::ABSOLUTE_PATH);
 
-
-    //  }
+        return new JsonResponse($jsonSCI, Response::HTTP_CREATED, ["Location" => $location], true);
+     }
 
     
 }
