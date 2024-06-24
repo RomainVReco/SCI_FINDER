5f7c2b96717f62e0469b91351fe9a954d6e83d3e
Romain VORLET <romain.vorlet@gmail.com>
2024-06-24 23:13:17 +0200

    fix if

--- src/Service/JsonScanner.php
+++ src/Service/JsonScanner.php
@@ -19,43 +19,43 @@ echo '<br/>';
 
 // for ($i = 0; $i<count($obj); $i++) {
 for ($i = 0; $i<3; $i++) {
-    $sci = new Sci();
-    $sci->setIdSCI($obj[$i]->id);
-    $sci->setSiren($obj[$i]->formality->siren);
-    $sci->setDateCreation(new DateTime($obj[$i]->formality->content->natureCreation->dateCreation));
-    $sci->setEtablieEnFrance($obj[$i]->formality->content->natureCreation->etablieEnFrance);
-    $sci->setSalarieEnFrance($obj[$i]->formality->content->natureCreation->salarieEnFrance);
-    $sci->setFormeJuridique($obj[$i]->formality->content->personneMorale->identite->entreprise->formeJuridique);
-    $sci->setDenomination($obj[$i]->formality->content->personneMorale->identite->entreprise->denomination);
-    $sci->setDateImmat(new DateTime($obj[$i]->formality->content->personneMorale->identite->entreprise->dateImmat));
-    $sci->setMontantCapital($obj[$i]->formality->content->personneMorale->identite->description->montantCapital);
-    $sci->setDeviseCapital($obj[$i]->formality->content->personneMorale->identite->description->montantCapital);
-
-    if (!empty($obj[$i]->formality->content->personneMorale->identite->description->objet)) {
-        $sci->setDescription($obj[$i]->formality->content->personneMorale->identite->description->objet);
+    if (!empty($obj[$i]->formality->content->personneMorale->identite->formeJuridique))
+        {
+            if (strcmp($obj[$i]->formality->content->personneMorale->identite->formeJuridique,"Société civile immobilière") || 
+            strcmp($obj[$i]->formality->content->personneMorale->identite->formeJuridique,"6540"))
+            {
+                $sci = new Sci();
+                $sci->setIdSCI($obj[$i]->id);
+                $sci->setSiren($obj[$i]->formality->siren);
+                $sci->setDateCreation(new DateTime($obj[$i]->formality->content->natureCreation->dateCreation));
+                $sci->setEtablieEnFrance($obj[$i]->formality->content->natureCreation->etablieEnFrance);
+                $sci->setSalarieEnFrance($obj[$i]->formality->content->natureCreation->salarieEnFrance);
+                $sci->setFormeJuridique($obj[$i]->formality->content->personneMorale->identite->entreprise->formeJuridique);
+                $sci->setDenomination($obj[$i]->formality->content->personneMorale->identite->entreprise->denomination);
+                $sci->setDateImmat(new DateTime($obj[$i]->formality->content->personneMorale->identite->entreprise->dateImmat));
+                $sci->setMontantCapital($obj[$i]->formality->content->personneMorale->identite->description->montantCapital);
+                $sci->setDeviseCapital($obj[$i]->formality->content->personneMorale->identite->description->montantCapital);
+            
+                if (!empty($obj[$i]->formality->content->personneMorale->identite->description->objet)) {
+                    $sci->setDescription($obj[$i]->formality->content->personneMorale->identite->description->objet);
+                }
+            
+                if (!empty($obj[$i]->formality->content->personneMorale->etablissementPrincipal)){
+                    $sci->setDateEffetFermeture(new DateTime($obj[$i]->formality->content->personneMorale->etablissementPrincipal->descriptionEtablissement->dateEffetFermeture));
+                } 
+                if (!empty($obj[$i]->formality->content->personneMorale->identite->entreprise->codeApe)){
+                    $sci->setCodeApe($obj[$i]->formality->content->personneMorale->identite->entreprise->codeApe);
+                }
+                $sci->setPositionInJson($i);
+                $sci->setFileName('madeup.json');
+                $arraySCI = (array) $sci;
+                $request = Request::create(
+                    '/api/sci',
+                    'POST',
+                    $arraySCI
+                );
+        }
     }
-
-
-    if (!empty($obj[$i]->formality->content->personneMorale->etablissementPrincipal)){
-        $sci->setDateEffetFermeture(new DateTime($obj[$i]->formality->content->personneMorale->etablissementPrincipal->descriptionEtablissement->dateEffetFermeture));
-    } 
-    if (!empty($obj[$i]->formality->content->personneMorale->identite->entreprise->codeApe)){
-        $sci->setCodeApe($obj[$i]->formality->content->personneMorale->identite->entreprise->codeApe);
-    }
-    $sci->setPositionInJson($i);
-    $sci->setFileName('madeup.json');
-    $arraySCI = (array) $sci;
-    // $request = Request::create(
-    //     '/api/sci',
-    //     'POST',
-    //     $arraySCI
-    // );
-
-    print_r($arraySCI);
-    echo '<br/>';
-    // $manager->persist($sci);
-    
 }
-
     // echo $obj[$i]->id;
     // echo $obj[$i]->formality->siren;
