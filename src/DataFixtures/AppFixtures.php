5d4f09eed62bf2f8c75ce14642dacf53d03dac56
Romain VORLET <rvorlet@partenorgroup.com>
2024-07-23 12:52:42 +0200

    ajout data fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Sci;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use DateTime;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i<30; $i++) {
+            $sci = new Sci();
+            $randomBytes = random_bytes(30);
+            $encodedBytes  = base64_encode($randomBytes);
+            $sci->setIdSCI($encodedBytes);
+            $sci->setSiren("SIREN ".$i);
+            $sci->setDateCreation(new DateTime());
+            $sci->setEtablieEnFrance(true);
+            $sci->setSalarieEnFrance(false);
+            $sci->setFormeJuridique("Forme juridique ".$i);
+            $sci->setDenomination("Denomination ".$i);
+            $sci->setDateImmat(new DateTime());
+            $sci->setMontantCapital(rand(1000, 150000));
+            $sci->setDeviseCapital("Devise capital ".$i);
+            $manager->persist($sci);
+        }
+        // $product = new Product();
+
+        $manager->flush();
+    }
+}
