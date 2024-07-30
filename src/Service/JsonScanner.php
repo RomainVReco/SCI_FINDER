b2bea0f09a96f47b421d319b763178a5ca156484
Romain VORLET <romain.vorlet@gmail.com>
2024-07-30 23:24:40 +0200

    revue attributs

--- src/Service/JsonScanner.php
+++ src/Service/JsonScanner.php
@@ -11,12 +11,6 @@ require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Entit
 require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
 
-function extractSci($obj):Sci {
-    $sci = new Sci();
-    return $sci;
-}
-
-
 $path = 'D:\Stockage\stock RNE formalité\\';
 $dir = scandir($path);
 print_r($dir);
@@ -36,6 +30,7 @@ for ($i = 2; $i<4; $i++) {
     echo(" \n");
     print_r("Nombre objets du fichier : " . count($obj));
     echo(" \n");
+    
     for ($j = 0 ; $j<count($obj); $j++) {
         $totalScan++;
     if (isset($obj[$j]->formality->content->natureCreation->formeJuridique))
@@ -47,6 +42,20 @@ for ($i = 2; $i<4; $i++) {
                 $sci = new Sci();
                 $sci->setIdSCI($obj[$j]->id);
                 $sci->setSiren($obj[$j]->formality->siren);
+                if (isset($obj[$i]->formality->content->natureCreation->dateCreation)) $sci->setDateCreation(new DateTime($obj[$i]->formality->content->natureCreation->dateCreation));
+                if (isset($obj[$i]->formality->content->natureCreation->etablieEnFrance)) $sci->setEtablieEnFrance($obj[$i]->formality->content->natureCreation->etablieEnFrance);
+                if (isset($obj[$i]->formality->content->natureCreation->salarieEnFrance)) $sci->setSalarieEnFrance($obj[$i]->formality->content->natureCreation->salarieEnFrance);
+                if (isset($obj[$i]->formality->content->personneMorale->identite->entreprise->formeJuridique)) $sci->setFormeJuridique($obj[$i]->formality->content->personneMorale->identite->entreprise->formeJuridique);
+                if (isset($obj[$i]->formality->content->personneMorale->identite->entreprise->denomination)) $sci->setDenomination($obj[$i]->formality->content->personneMorale->identite->entreprise->denomination);
+                if (isset($obj[$i]->formality->content->personneMorale->identite->entreprise->denomination)) $sci->setDenomination($obj[$i]->formality->content->personneMorale->identite->entreprise->denomination);
+                if (isset($obj[$i]->formality->content->personneMorale->identite->entreprise->dateImmat)) $sci->setDateImmat(new DateTime($obj[$i]->formality->content->personneMorale->identite->entreprise->dateImmat));
+                if (isset($obj[$i]->formality->content->personneMorale->identite->description->montantCapital)) $sci->setMontantCapital($obj[$i]->formality->content->personneMorale->identite->description->montantCapital);
+                if (isset($obj[$i]->formality->content->personneMorale->identite->description->deviseCapital)) $sci->setDeviseCapital($obj[$i]->formality->content->personneMorale->identite->description->deviseCapital);
+                
+                if (isset($obj[$i]->formality->content->personneMorale->identite->description->objet)) $sci->setDescription($obj[$i]->formality->content->personneMorale->identite->description->objet);
+                if (isset($obj[$i]->formality->content->personneMorale->etablissementPrincipal)) $sci->setDateEffetFermeture(new DateTime($obj[$i]->formality->content->personneMorale->etablissementPrincipal->descriptionEtablissement->dateEffetFermeture));
+                if (isset($obj[$i]->formality->content->personneMorale->identite->entreprise->codeApe)) $sci->setCodeApe($obj[$i]->formality->content->personneMorale->identite->entreprise->codeApe);
+                
                 $sci->setPositionInJson($j);
                 $sci->setFileName($fileName);
                 $setSci->add($sci);
